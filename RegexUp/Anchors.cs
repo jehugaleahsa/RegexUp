a1005d668bf2797687f1b96c1ec5f0ce638cb371
Travis Parks <jehugaleahsa@gmail.com>
2018-10-21 22:13:59 -0400

    Anchors using forward slashes.

--- RegexUp/Anchors.cs
+++ RegexUp/Anchors.cs
@@ -18,31 +18,31 @@
         /// <summary>
         /// The match must occur at the start of the string.
         /// </summary>
-        public static IAnchor A { get; } = new Anchor(@"/A");
+        public static IAnchor A { get; } = new Anchor(@"\A");
 
         /// <summary>
         /// The match must occur at the end of the string or before \n at the end of the string.
         /// </summary>
-        public static IAnchor Z { get; } = new Anchor(@"/Z");
+        public static IAnchor Z { get; } = new Anchor(@"\Z");
 
         /// <summary>
         /// The match must occur at the end of the string.
         /// </summary>
-        public static IAnchor z { get; } = new Anchor(@"/z");
+        public static IAnchor z { get; } = new Anchor(@"\z");
 
         /// <summary>
         /// The match must occur at the point where the previous match ended.
         /// </summary>
-        public static IAnchor G { get; } = new Anchor(@"/G");
+        public static IAnchor G { get; } = new Anchor(@"\G");
 
         /// <summary>
         /// The match must occur on a boundary between a \w (alphanumeric) and a \W (nonalphanumeric) character.
         /// </summary>
-        public static IAnchor b { get; } = new Anchor(@"/b");
+        public static IAnchor b { get; } = new Anchor(@"\b");
 
         /// <summary>
         /// The match must not occur on a \b boundary.
         /// </summary>
-        public static IAnchor B { get; } = new Anchor(@"/B");
+        public static IAnchor B { get; } = new Anchor(@"\B");
     }
 }
